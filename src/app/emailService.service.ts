39b0f3abb6324e819f47d6c2ac8e478dc98227c4
guialvesds <guilherme_ealves@outlook.com>
2023-09-04 15:58:17 -0300

    variável de ambiente

--- src/app/emailService.service.ts
+++ src/app/emailService.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import * as emailjs from 'emailjs-com';
+import { environment } from 'src/environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  private user_id = 'Sav0Eb5WjwqVKg5Fi';
-  private template_id = 'template_f7uwblp';
+  private user_id = environment.USER_ID;
+  private template_id = environment.TEMPLATE_ID;
 
   constructor() {
     emailjs.init(this.user_id);
@@ -15,7 +18,7 @@ export class EmailService {
 
   enviarEmail(nome: string, email: string, phone: string,  mensagem: string) {
     const params = {
-      to_name: 'Destinatário',
+      to_name: 'FlexDuck',
       from_name: nome,
       message: mensagem,
       reply_to: email,
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  USER_ID: 'Sav0Eb5WjwqVKg5Fi',
+  TEMPLATE_ID: 'template_f7uwblp',
+};
